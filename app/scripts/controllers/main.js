b7a645417faafc9f4226cef1b5471db917655844
Will Ong <travellingwill@gmail.com>
2015-12-01 22:51:17 +1100

    Scaffolded an angular app with Yeoman - initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myStoreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myStoreApp
+ */
+angular.module('myStoreApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
